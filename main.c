adf0d519969366aeb9e9c6c07ff3ea13b44c6b21
Желудков Давид <ip011s13@bwp06.csc.local>
2021-02-20 10:46:45 +0700

    Change main.c

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
-int main(){
-  printf("meta charset=\"utf-8\">\n");
-  printf("<style type=\"text/css\">\n");
-  printf(".white.king:before {content: \"\\2654\";}\n");
-  printf("</style>\n");
-  return 0;
+int main()
+{
+    printf("meta charset=\"utf-8\">\n");
+    printf("<style type=\"text/css\">\n");
+    printf(".white.king:before {content: \"\\2654\";}\n");
+    printf("</style>\n");
+    return 0;
 }
