674e8a5fdc7d420170fa2f3eb7e3b99aaf3296e9
Желудков Давид <ip011s13@bwp06.csc.local>
2021-02-20 10:34:14 +0700

    Change main.c

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+  printf("meta charset=\"utf-8\">\n");
+  printf("<style type=\"text/css\">\n");
+  printf(".white.king:before {content: \"\\2654\";}\n");
+  printf("</style>\n");
+  return 0;
+}
